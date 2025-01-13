37212eabc7be44896993cd269dd8733f192ba901
Gustavo Leite <gustavoleite.ti@gmail.com>
2025-01-13 17:47:41 -0300

    [RISCV][MPE] Add arithmetic instructions

--- llvm/test/MC/RISCV/xmpe-valid.s
+++ llvm/test/MC/RISCV/xmpe-valid.s
@@ -11,3 +11,39 @@ ml      m20, (x7)
 # CHECK-ASM-AND-OBJ: ms m5, (t0)
 # CHECK-ASM: encoding: [0xf7,0x92,0x02,0x22]
 ms      m5, (x5)
+
+# CHECK-ASM-AND-OBJ: mmac m1, m2, m3
+# CHECK-ASM: encoding: [0xf7,0x00,0x31,0x66]
+mmac    m1, m2, m3
+
+# CHECK-ASM-AND-OBJ: mmacu m1, m2, m3
+# CHECK-ASM: encoding: [0xf7,0x00,0x31,0x62]
+mmacu   m1, m2, m3
+
+# CHECK-ASM-AND-OBJ: mmacf m1, m2, m3
+# CHECK-ASM: encoding: [0xf7,0x00,0x31,0x6a]
+mmacf   m1, m2, m3
+
+# CHECK-ASM-AND-OBJ: madd m1, m2, m3
+# CHECK-ASM: encoding: [0xf7,0x10,0x31,0x66]
+madd    m1, m2, m3
+
+# CHECK-ASM-AND-OBJ: maddu m1, m2, m3
+# CHECK-ASM: encoding: [0xf7,0x10,0x31,0x62]
+maddu   m1, m2, m3
+
+# CHECK-ASM-AND-OBJ: maddf m1, m2, m3
+# CHECK-ASM: encoding: [0xf7,0x10,0x31,0x6a]
+maddf   m1, m2, m3
+
+# CHECK-ASM-AND-OBJ: msub m1, m2, m3
+# CHECK-ASM: encoding: [0xf7,0x20,0x31,0x66]
+msub    m1, m2, m3
+
+# CHECK-ASM-AND-OBJ: msubu m1, m2, m3
+# CHECK-ASM: encoding: [0xf7,0x20,0x31,0x62]
+msubu   m1, m2, m3
+
+# CHECK-ASM-AND-OBJ: msubf m1, m2, m3
+# CHECK-ASM: encoding: [0xf7,0x20,0x31,0x6a]
+msubf   m1, m2, m3
