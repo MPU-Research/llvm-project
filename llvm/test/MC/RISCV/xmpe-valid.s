a5ee2cd825b6dba39843d1c1f28a91b63c71cdba
Gustavo Leite <gustavoleite.ti@gmail.com>
2025-01-13 19:02:25 -0300

    [RISCV][MPE] Add move instructions

--- llvm/test/MC/RISCV/xmpe-valid.s
+++ llvm/test/MC/RISCV/xmpe-valid.s
@@ -47,3 +47,39 @@ msubu   m1, m2, m3
 # CHECK-ASM-AND-OBJ: msubf m1, m2, m3
 # CHECK-ASM: encoding: [0xf7,0x20,0x31,0x6a]
 msubf   m1, m2, m3
+
+# CHECK-ASM-AND-OBJ: mmv.mm m1, m2
+# CHECK-ASM: encoding: [0xf7,0x00,0x01,0x42]
+mmv.mm m1, m2
+
+# CHECK-ASM-AND-OBJ: mmv.xm m1, sp
+# CHECK-ASM: encoding: [0xf7,0x00,0x11,0x42]
+mmv.xm m1, x2
+
+# CHECK-ASM-AND-OBJ: mmv.zx ra, m2
+# CHECK-ASM: encoding: [0xf7,0x00,0x21,0x42]
+mmv.zx x1, m2
+
+# CHECK-ASM-AND-OBJ: mmv.zm m1, m2
+# CHECK-ASM: encoding: [0xf7,0x00,0x41,0x42]
+mmv.zm m1, m2
+
+# CHECK-ASM-AND-OBJ: mmv.fm m1, ft2
+# CHECK-ASM: encoding: [0xf7,0x00,0x51,0x42]
+mmv.fm m1, f2
+
+# CHECK-ASM-AND-OBJ: mmv.zf ft1, m2
+# CHECK-ASM: encoding: [0xf7,0x00,0x61,0x42]
+mmv.zf f1, m2
+
+# CHECK-ASM-AND-OBJ: mmv.fz m1, ft2
+# CHECK-ASM: encoding: [0xf7,0x00,0x71,0x42]
+mmv.fz m1, f2
+
+# CHECK-ASM-AND-OBJ: mmv.xm m1, zero
+# CHECK-ASM: encoding: [0xf7,0x00,0x10,0x42]
+mzero  m1
+
+# CHECK-ASM-AND-OBJ: mmv.xm m2, zero
+# CHECK-ASM: encoding: [0x77,0x01,0x10,0x42]
+mz  m2
